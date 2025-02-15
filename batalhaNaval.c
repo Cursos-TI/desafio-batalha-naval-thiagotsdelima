361a421df2bfa6f256ba50cb0c29088ee63862f0
thiago <thiagotsdelima@outlook.com>
2025-02-15 16:39:29 -0300

    finished

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,35 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[5][5] = {
+    {1, 2, 3, 4, 5},
+    {6, 7, 8, 9, 10},
+    {11, 12, 13, 14, 15},
+    {16, 17, 18, 19, 20},
+    {21, 22, 23, 24, 25}
+    };
+    int escolha;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    printf("***Escolha o posicionamento do navio***\n");
+    printf("1. Navio vertical\n");
+    printf("2. Navio horizontal\n");
+    printf("3. Sair\n");
+    printf("Digite sua escolha: ");
+    scanf("%d", &escolha);
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+    switch(escolha) { 
+        case 1:
+            printf("You chose the option Boat vertically! %d\n", tabuleiro[3][2]);
+            break;
+        case 2:
+           printf("You chose the option Boat vertically! %d\n", tabuleiro[2][4]);
+            break;
+        case 3:
+            printf("Saindo...\n");
+            return 0;
+        default:
+            printf("Opção inválida!\n");
+            return 1;
+    }
     return 0;
 }
